9a92f7dd486332616177073cfe3c7d15f7d72624
Giovanni Fonseca Mejía <51707590+kk-gfonseca@users.noreply.github.com>
2020-07-04 13:31:35 -0600

    Aplicacion Inv
    Aplicacion de Inventario con la tabla inicial, no hay datos para mostrar.

--- app/bases/models.py
+++ app/bases/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField("Estado", default=True)
+    fecha_Creacion = models.DateTimeField("Fecha Creacion", auto_now_add= True)
+    fecha_Modificado = models.DateTimeField("Fecha Modificacion", auto_now=True)
+    usuario_Crea = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_Modifica = models.IntegerField("Usuario Modifica", null=True, blank=True)
+
+    class Meta:
+        abstract=True 
--- app/bases/urls.py
+++ app/bases/urls.py
@@ -8,5 +8,5 @@ from bases.views import Home
 urlpatterns = [
     path('',Home.as_view(), name='home'),
     path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),    
 ]
