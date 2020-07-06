53e87517485d60669e0d22b732776841c599a4b0
Giovanni Fonseca Mejía <51707590+kk-gfonseca@users.noreply.github.com>
2020-07-05 20:36:22 -0600

    Categoria edicion y datatable
    Se agrago la edicion y la funcionalidad de datatable

--- app/inv/views.py
+++ app/inv/views.py
@@ -24,3 +24,29 @@ class CategoriaNew(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.usuario_Crea = self.request.user
         return super().form_valid(form)
+
+
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    form_class = CategoriaForm
+    success_url = reverse_lazy("inv:categoria_list")
+    login_url = "bases:login"
+    
+    def form_valid(self, form):
+        form.instance.usuario_Modifica = self.request.user.id
+        return super().form_valid(form)
+
+
+class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    form_class = CategoriaForm
+    success_url = reverse_lazy("inv:categoria_list")
+    login_url = "bases:login"
+    
+    def form_valid(self, form):
+        form.instance.usuario_Modifica = self.request.user.id
+        return super().form_valid(form)
